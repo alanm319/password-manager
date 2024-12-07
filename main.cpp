7de08ffc5ac3ed5687b34f13f4743a09fe38ab7e
Alan <alanm319@gmail.com>
2024-12-06 22:02:50 -0600

    dek and kek derivation

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 #include <sys/stat.h>
 #include <filesystem>
 
+const unsigned int DEK_LEN = 32U;
+const unsigned int CIPHERTEXT_LEN = crypto_secretbox_MACBYTES + DEK_LEN;
+
 void to_hex(const unsigned char *const data, size_t size)
 {
     for (size_t i = 0; i < size; i++)
@@ -47,8 +50,8 @@ std::string get_password(const std::string &prompt)
 }
 
 
-// TODO deprecate
-void save_salt_and_hash(const std::vector<unsigned char> &salt, const std::vector<unsigned char> &key_hash)
+// stores salt plaintext, nonce plaintext, and dek (ciphertext)
+void create_key_file(const std::vector<unsigned char> &salt, const std::vector<unsigned char> &nonce, const std::vector<unsigned char> &cipher)
 {
     std::ofstream out("key.bin", std::ios::binary);
     if (!out) {
@@ -56,14 +59,15 @@ void save_salt_and_hash(const std::vector<unsigned char> &salt, const std::vecto
     }
 
     out.write(reinterpret_cast<const char *>(salt.data()), salt.size());
-    out.write(reinterpret_cast<const char *>(key_hash.data()), key_hash.size());
-    out.close();
+    out.write(reinterpret_cast<const char *>(nonce.data()), nonce.size());
+    out.write(reinterpret_cast<const char *>(cipher.data()), cipher.size());
 
+    out.close();
     chmod("key.bin", S_IRUSR | S_IWUSR);
 }
 
-// TODO deprecate
-void read_salt_and_hash(std::vector<unsigned char> &salt, std::vector<unsigned char> &key_hash)
+// reads the plaintext salt and nonce and encrypted dek for decryption
+void read_key_file(std::vector<unsigned char> &salt, std::vector<unsigned char> &nonce, std::vector<unsigned char> &cipher)
 {
     std::ifstream in("key.bin", std::ios::binary);
     if (!in) {
@@ -71,12 +75,47 @@ void read_salt_and_hash(std::vector<unsigned char> &salt, std::vector<unsigned c
     }
 
     salt.resize(crypto_pwhash_SALTBYTES);
-    key_hash.resize(crypto_generichash_BYTES);
+    nonce.resize(crypto_secretbox_NONCEBYTES);
+    cipher.resize(CIPHERTEXT_LEN);
 
     in.read(reinterpret_cast<char *>(salt.data()), salt.size());
-    in.read(reinterpret_cast<char *>(key_hash.data()), key_hash.size());
+    in.read(reinterpret_cast<char *>(nonce.data()), nonce.size());
+    in.read(reinterpret_cast<char *>(cipher.data()), cipher.size());
 
     in.close();
+}
+
+// TODO store sensitive data in secure memory
+void login_usr() {
+    // TODO store in secure memory
+    // prompt the user for a login password
+    const std::string password = get_password("Enter master password: ");
+
+    // retrieve the stored salt, nonce, and encrypted
+    std::vector<unsigned char> stored_salt, stored_nonce, stored_cipher;
+    read_key_file(stored_salt, stored_nonce, stored_cipher);
+
+    // derive the key and hash it
+    std::vector<unsigned char> key(crypto_secretbox_KEYBYTES);
+
+    if (crypto_pwhash(key.data(), key.size(),
+                        password.c_str(), password.length(), stored_salt.data(),
+                        crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE,
+                        crypto_pwhash_ALG_DEFAULT) != 0) {
+        throw std::runtime_error("Key derivation failed");
+    }
+
+
+
+    // decrypt dek
+    std::vector<unsigned char> decrypted(DEK_LEN);
+    if (crypto_secretbox_open_easy(decrypted.data(), stored_cipher.data(), CIPHERTEXT_LEN, stored_nonce.data(), key.data()) != 0) {
+    /* message forged! */
+        throw std::runtime_error("Authintication failed");
+    } else {
+        std::cout << "Authentication successful!" << std::endl;
+    }
+
 }
 
 // TODO store sensitive data in secure memory
@@ -85,17 +124,15 @@ void first_time_setup() {
     std::cout << "Welcome to password manager!!!" << std::endl;
 
     // ask for password
-    const std::string password = get_password("Enter a password: ");
+    std::string password = get_password("Set a password: ");
 
     // generate a salt
     std::vector<unsigned char> salt(crypto_pwhash_SALTBYTES);
     randombytes_buf(salt.data(), salt.size());
 
     // generate key
-    // TODO change key len to 256
-    std::vector<unsigned char> key(crypto_pwhash_BYTES_MIN);
+    std::vector<unsigned char> key(crypto_secretbox_KEYBYTES);
 
-    // TODO use argon2id and a higher opslimit
     if (crypto_pwhash(key.data(), key.size(),
                         password.c_str(), password.length(), salt.data(),
                         crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE,
@@ -105,57 +142,22 @@ void first_time_setup() {
         throw std::runtime_error("Key derivation failed");
     }
 
+    sodium_memzero(&password[0], password.size());
 
+    // generate a noce for dek encryption
+    std::vector<unsigned char> nonce(crypto_secretbox_NONCEBYTES);
+    randombytes_buf(nonce.data(), nonce.size());
 
-    // TODO instead of saving a hash of key, derive a DEK and encrypt it the key (KEK)
-    // hash the key
-    std::vector<unsigned char> key_hash(crypto_generichash_BYTES);
-    crypto_generichash(key_hash.data(), key_hash.size(), key.data(), key.size(), nullptr, 0);
+    // generate dek
+    std::vector<unsigned char> dek(DEK_LEN);
+    randombytes_buf(dek.data(), dek.size());
 
-    // store the salt and key hash
-    save_salt_and_hash(salt, key_hash);
+    // ecnrypt dek
+    std::vector<unsigned char> ciphertext(CIPHERTEXT_LEN);
+    crypto_secretbox_easy(ciphertext.data(), dek.data(), dek.size(), nonce.data(), key.data());
 
-    // verify salt and key hash
-    std::vector<unsigned char> stored_salt, stored_hash;
-    read_salt_and_hash(stored_salt, stored_hash);
-
-    if (salt != stored_salt || key_hash != stored_hash) {
-        throw std::runtime_error("Verification Failed: Stored data does not match");
-    }
-    std::cout << "First-time setup complete" << std::endl;
-}
-
-// TODO store sensitive data in secure memory
-void login_usr() {
-    // TODO store in secure memory
-    // prompt the user for a login password
-    const std::string password = get_password("Enter master password: ");
-
-    // retrieve the stored salt and key hash
-    std::vector<unsigned char> stored_salt, stored_key_hash;
-    read_salt_and_hash(stored_salt, stored_key_hash);
-
-    // derive the key and hash it
-    std::vector<unsigned char> key(crypto_pwhash_BYTES_MIN);
-
-    // TODO use same parameters for kdf as first_time_setup()
-    if (crypto_pwhash(key.data(), key.size(),
-                        password.c_str(), password.length(), stored_salt.data(),
-                        crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE,
-                        crypto_pwhash_ALG_DEFAULT) != 0) {
-        throw std::runtime_error("Key derivation failed");
-    }
-
-    // TODO update to new architecutre
-    std::vector<unsigned char> derived_key_hash(crypto_generichash_BYTES);
-    crypto_generichash(derived_key_hash.data(), derived_key_hash.size(), key.data(), key.size(), nullptr, 0);
-
-    // check if the stored key hash matches the derived hash and act accordingly
-    if (stored_key_hash == derived_key_hash) {
-        std::cout << "Login successful" << std::endl;
-    } else {
-        std::cout << "Login failed" << std::endl;
-    }
+    // write the encrypted file into a file (for now)
+    create_key_file(salt, nonce, ciphertext);
 }
 
 int main()
@@ -169,10 +171,9 @@ int main()
 
         if (!std::filesystem::exists("key.bin")) {
             first_time_setup();
-        } 
+        }
 
         login_usr();
-        
 
     }
     catch (const std::exception &e) {
